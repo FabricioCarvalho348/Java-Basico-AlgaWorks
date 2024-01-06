862ce59d0575888572ff14363547332123d83aff
Fabricio Carvalho <fabricioccarvalho348@gmail.com>
2024-01-06 08:32:41 -0300

    feat: Criando um algoritmo de lista, removendo elementos de uma lista, adicionando elementos em uma lista e ordenando a lista e um exercicio fazendo essa ordenação em elementos dentro de um arquivo.

---
+++ src/algoritmosavancados/Aluno.java
@@ -0,0 +1,13 @@
+package algoritmosavancados;
+
+public class Aluno {
+
+    String nome;
+
+    boolean vemDepoisDe(Aluno aluno) {
+        if(aluno == null) {
+            return false;
+        }
+        return nome.compareTo(aluno.nome) > 0;
+    }
+}
